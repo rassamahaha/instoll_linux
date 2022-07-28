05752035ceba070bc4d5c0d74c26956a322b95b5
rassamahaha <rassamahakill@gmail.com>
2022-07-28 10:50:43 +0300

    Add mysistem.sh

---
+++ mysistem.sh
@@ -0,0 +1,16 @@
+sudo install vim
+
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install ./google-chrome-stable_current_amd64.deb
+
+sudo snap install telegram-desktop
+
+sudo apt-get install zsh
+whereis zsh
+
+
+sudo apt install git
+
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+
